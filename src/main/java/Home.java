b1d4666d53c187da222a577e6f26c40a4170020f
zedsdead95 <mr.walid.koubaa@gmail.com>
2019-03-18 10:30:54 +0100

    Database mais soucis avec le driver

--- src/main/java/Home.java
+++ src/main/java/Home.java
@@ -1,10 +1,15 @@
+import database.DatabaseController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Home extends Application {
 
    public static void main(String[] args) {
+
+
       System.out.println("COUCOU");
+      DatabaseController.connexion();
+      DatabaseController.search();
    }
 
    @Override
--- src/main/java/database/DatabaseController.java
+++ src/main/java/database/DatabaseController.java
@@ -1,6 +1,42 @@
 package database;
 
+import java.sql.*;
+
 public class DatabaseController {
 
-    
+    private static Connection mConnection;
+    private static String url = "jdbc:mysql://localhost:9999/pro2019?user=user&password=password";
+
+
+    public static void connexion() {
+        try {
+            mConnection = DriverManager.getConnection(url);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    public static ResultSet search() {
+        Statement statement;
+        //PreparedStatement preparedStatement = null;
+        try {
+
+
+            statement = mConnection.createStatement();
+
+            String sql = " SELECT *"
+                    +" FROM USER ;";
+
+            ResultSet result = statement.executeQuery(sql);
+            System.out.println(result.toString());
+            return result;
+
+        } catch (Exception e) {
+            System.out.println("erreur query1");
+            return null;
+        }
+
+    }
 }
