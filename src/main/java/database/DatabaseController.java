ed066ba44f9b9ecdcbd559fffba15feaa61c578f
Adam <adam.zouari@heig-vd.ch>
2019-03-25 13:59:05 +0100

    connection working

--- src/main/java/database/DatabaseController.java
+++ src/main/java/database/DatabaseController.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DatabaseController {
 
     private static Connection mConnection;
-    private static String url = "jdbc:mysql://localhost:9999/pro2019?user=user&password=password";
+    private static String url = "jdbc:mysql://localhost:3308/pro2019?user=user&password=password";
 
 
     public static void connexion() {
@@ -27,14 +27,25 @@ public class DatabaseController {
             statement = mConnection.createStatement();
 
             String sql = " SELECT *"
-                    +" FROM USER ;";
+                    +" FROM user ;";
 
             ResultSet result = statement.executeQuery(sql);
-            System.out.println(result.toString());
+
+            ResultSetMetaData rsmd = result.getMetaData();
+
+            int columnsNumber = rsmd.getColumnCount();
+            while (result.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = result.getString(i);
+                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                }
+                System.out.println("");
+            }
             return result;
 
         } catch (Exception e) {
-            System.out.println("erreur query1");
+            System.out.println("erreur query1" + e.getMessage());
             return null;
         }
 
