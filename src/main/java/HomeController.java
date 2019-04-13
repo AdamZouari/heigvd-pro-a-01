5d0ba8528bf2012cdfb997aa21abc3b6d9c20477
Christoph Rueff <christoph.rueff@gmail.com>
2019-04-13 10:17:58 +0200

    Add RulesView.fxml and SettingsView.fxml files, Add RulesController and SettingsController

--- src/main/java/HomeController.java
+++ src/main/java/HomeController.java
@@ -4,7 +4,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
