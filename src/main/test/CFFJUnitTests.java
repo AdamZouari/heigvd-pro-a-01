291ee144880db705c26b36103f01b792190a2bf5
zedsdead95 <mr.walid.koubaa@gmail.com>
2019-05-09 14:22:37 +0200

    test ajout

--- src/main/test/CFFJUnitTests.java
+++ src/main/test/CFFJUnitTests.java
@@ -1,16 +1,31 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import Utils.JsonParserCFF;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
+import service.ServiceCFF;
+
+import java.io.FileNotFoundException;
 
 public class CFFJUnitTests {
 
+    ServiceCFF cff = new ServiceCFF();
     @Test
     public void connnectionTOAPIisOK(){
 
+        //assert(cff.connect(),"");
     }
 
     @Test
-    public void testCFFConnectionsBetweenTwoCities(){
+    public void testCFFConnectionsBetweenTwoCities() throws FileNotFoundException, ParseException {
         assertEquals(2,1+1);
+
+        System.out.println("Voici toutes les connexions possibles entre Lausanne et Geneve:");
+        String connectionsLtoG = cff.getTrainsForPath("Lausanne","Geneve","2012-03-25","17:30");
+        System.out.println(connectionsLtoG);
+
+        JsonParserCFF.parseCFF(connectionsLtoG);
+
+        cff.disconnect();
     }
 }
