4ee9f005046d635a44d404261d40b94f2b4e5904
Adam <adam.zouari@heig-vd.ch>
2019-05-10 18:08:46 +0200

    create a simple server

--- PRO-server/src/main/java/Server.java
+++ PRO-server/src/main/java/Server.java
@@ -1,8 +1,108 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Server {
 
-    public static void main(String[] args) {
+    final static Logger LOG = Logger.getLogger(Server.class.getName());
 
-        System.out.println("This is the server");
+    public void serveClients() {
+        new Thread(new ReceptionistWorker()).start();
     }
 
+    private class ReceptionistWorker implements Runnable {
+
+        @Override
+        public void run() {
+            ServerSocket serverSocket;
+
+            try {
+                serverSocket = new ServerSocket(9999);
+            } catch (IOException ex) {
+                LOG.log(Level.SEVERE, null, ex);
+                return;
+            }
+
+            while (true) {
+                LOG.info("Waiting (blocking) for a new client...");
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    new Thread(new ServantWorker(clientSocket)).start();
+                } catch (IOException ex) {
+                    LOG.log(Level.SEVERE, ex.getMessage(), ex);
+                }
+            }
+
+        }
+
+        private class ServantWorker implements Runnable {
+
+            Socket clientSocket;
+            BufferedReader in = null;
+            PrintWriter out = null;
+
+            public ServantWorker(Socket clientSocket) {
+                try {
+                    this.clientSocket = clientSocket;
+                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    out = new PrintWriter(clientSocket.getOutputStream());
+                } catch (IOException ex) {
+                    LOG.log(Level.SEVERE, ex.getMessage(), ex);
+                }
+            }
+
+            @Override
+            public void run() {
+                String line;
+                boolean shouldRun = true;
+
+                try {
+                    LOG.info("Reading until client sends BYE or closes the connection...");
+                    //TODO CHANGER COMPORTEMENT, RENVOIE SEULEMENT CE QUE LE CLIENT LUI ENVOI
+                    while ((shouldRun) && (line = in.readLine()) != null) {
+                        if (line.equalsIgnoreCase("bye")) {
+                            shouldRun = false;
+                        }
+                        out.println("> " + line.toUpperCase());
+                        out.flush();
+                    }
+
+                    LOG.info("Cleaning up resources...");
+                    in.close();
+                    out.close();
+                    clientSocket.close();
+
+                } catch (IOException ex) {
+                    if (in != null) {
+                        try {
+                            in.close();
+                        } catch (IOException ex1) {
+                            LOG.log(Level.SEVERE, ex.getMessage(), ex1);
+                        }
+                    }
+                    if (out != null) {
+                        out.close();
+                    }
+                    if (clientSocket != null) {
+                        try {
+                            clientSocket.close();
+                        } catch (IOException ex1) {
+                            LOG.log(Level.SEVERE, ex1.getMessage(), ex1);
+                        }
+                    }
+                    LOG.log(Level.SEVERE, ex.getMessage(), ex);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is the server");
+        new Server().serveClients();
+    }
 }
