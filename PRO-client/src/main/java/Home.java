b7fdd57ce51e379bc3dff1571f719a9669955e9a
David Jaquet <djaquet5@gmail.com>
2019-05-11 19:35:06 +0200

    I18N fonctionne, faut encore rajouter des checkdowns

--- PRO-client/src/main/java/Home.java
+++ PRO-client/src/main/java/Home.java
@@ -5,6 +5,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 
 public class Home extends Application {
 
@@ -14,14 +18,22 @@ public class Home extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(this.getClass().getResource("/LoginView.fxml"));
-//        Parent root = FXMLLoader.load(this.getClass().getResource("/HomeView.fxml"));
+        try {
+//            FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/HomeView.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/LoginView.fxml"));
+            fxmlLoader.setResources(ResourceBundle.getBundle("Internationalization", new Locale("fr", "FR")));
 
-        stage.setScene(new Scene(root));
-        stage.setResizable(false);
-        stage.setTitle("ASAPP");
-        stage.show();
+            Parent root = fxmlLoader.load();
 
-        stage.setOnCloseRequest(event -> Platform.exit());
+            stage.setScene(new Scene(root));
+            stage.setResizable(false);
+            stage.setTitle("ASAPP");
+            stage.show();
+
+            stage.setOnCloseRequest(event -> Platform.exit());
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- PRO-client/src/main/java/controller/LoginController.java
+++ PRO-client/src/main/java/controller/LoginController.java
@@ -2,13 +2,22 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable {
+   private ResourceBundle bundle;
+
+   @FXML
+   private Label usernameLabel;
+
    @FXML
    private void onSignUpClick() {
       try {
@@ -26,5 +35,11 @@ public class LoginController {
    @FXML
    private void onLoginButtonClick() {
 
+   }
+
+   @Override
+   public void initialize(URL location, ResourceBundle resources) {
+      bundle = resources;
+//      usernameLabel.setText(bundle.getString("username"));
    }
 }
