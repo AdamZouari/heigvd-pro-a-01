a551fe242fa526ad3f4f0da89b4ae71461ed1352
Adam <adam.zouari@heig-vd.ch>
2019-05-11 12:20:30 +0200

    add common module

---
+++ PRO-common/src/main/java/protocol/Protocol.java
@@ -0,0 +1,18 @@
+package protocol;
+
+public class Protocol {
+
+    public final static int DEFAULT_PORT = 1111;
+
+    public final static char EOL = '\n';
+
+    public final static String CMD_LOG = "LOG";
+    public final static String CMD_REG = "REG";
+
+    public final static String CMD_GET_RES_RULES = "GET_RES_RULES";
+    public final static String CMD_GET_RULES = "GET_RULES";
+
+    public final static String CMD_CREATE_RULE = "CREATE_RULE";
+    public final static String CMD_SEND_RULE = "SEND_RULE";
+
+}
