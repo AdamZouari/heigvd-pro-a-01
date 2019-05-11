23823511ac938b0b0db2d6c174ad6235805a8534
David Jaquet <djaquet5@gmail.com>
2019-05-11 16:00:12 +0200

    Edit login and register view

--- PRO-client/src/main/java/Home.java
+++ PRO-client/src/main/java/Home.java
@@ -1,11 +1,9 @@
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 
 public class Home extends Application {
@@ -16,7 +14,8 @@ public class Home extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(this.getClass().getResource("/LoginView.fxml"));
+//        Parent root = FXMLLoader.load(this.getClass().getResource("/LoginView.fxml"));
+        Parent root = FXMLLoader.load(this.getClass().getResource("/HomeView.fxml"));
 
         stage.setScene(new Scene(root));
         stage.setTitle("ASAPP");
