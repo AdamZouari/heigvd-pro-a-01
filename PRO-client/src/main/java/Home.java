68188ecab8ee1dfb4762cab07f41b4b810c46f71
David Jaquet <djaquet5@gmail.com>
2019-05-11 12:40:27 +0200

    Ferme les fenêtres quand on ferme la fenêtre principale

--- PRO-client/src/main/java/Home.java
+++ PRO-client/src/main/java/Home.java
@@ -1,8 +1,11 @@
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 
 public class Home extends Application {
@@ -13,11 +16,17 @@ public class Home extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(this.getClass().getResource("/HomeView.fxml"));
+        Parent root = FXMLLoader.load(this.getClass().getResource("/LoginView.fxml"));
 
         stage.setScene(new Scene(root));
-        stage.setTitle("AservA");
+        stage.setTitle("ASAPP");
         stage.show();
+
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent event) {
+                Platform.exit();
+            }
+        });
     }
 
 }
