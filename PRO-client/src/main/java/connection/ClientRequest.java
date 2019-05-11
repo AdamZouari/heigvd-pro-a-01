70417fe0d5589cdc20e664f6eecd1d57652cc7a8
zedsdead95 <mr.walid.koubaa@gmail.com>
2019-05-11 13:07:15 +0200

    client request added

---
+++ PRO-client/src/main/java/connection/ClientRequest.java
@@ -0,0 +1,42 @@
+package connection;
+
+import java.net.Socket;
+
+public class ClientRequest {
+
+    Socket socket = new Socket();
+
+    public void connect(){
+
+    }
+
+    public void register(){
+
+    }
+
+    public void login(){
+
+        // if connection ok then connect
+
+        // else refuse connection
+    }
+
+    // the rules result for
+    public void getRulesResult(String username){
+        //
+    }
+
+    // the rules content
+    public void getRulesContent(String username){
+
+    }
+
+    public void createNewRule(){
+
+    }
+
+    public void sendRule(){
+
+    }
+
+}
