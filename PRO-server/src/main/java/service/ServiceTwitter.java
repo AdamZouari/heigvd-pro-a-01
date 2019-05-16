adb3ebb150c9e1e821ac260d64e243e08942ba64
antoine.nourazar@heig-vd.ch <antoine.nourazar@heig-vd.ch>
2019-05-16 14:48:33 +0200

    connection twitter okay but need an ui to redirect to twitter site

--- PRO-server/src/main/java/service/ServiceTwitter.java
+++ PRO-server/src/main/java/service/ServiceTwitter.java
@@ -1,27 +1,79 @@
 package service;
 
 
+import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 
 public class ServiceTwitter extends Service {
 
-    Twitter twitter;
-    HttpURLConnection connection;
+    private Twitter twitter;
+    private HttpURLConnection connection;
+    private ConfigurationBuilder cb;
+
+    public ServiceTwitter() {
+        cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("n9E1yBbhc1hSBGj61iU3lvGT6")
+                .setOAuthConsumerSecret("b36cQpIffHxvsG6BofnvxX9l9x488e0hxEd5sx1bwthmKmddSJ");
+    }
 
     @Override
     public void connect() {
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("n9E1yBbhc1hSBGj61iU3lvGT6 ")
-                .setOAuthConsumerSecret("b36cQpIffHxvsG6BofnvxX9l9x488e0hxEd5sx1bwthmKmddSJ")
-                .setOAuthAccessToken("194071924-djw0w4pI0zbxkFVZngE7J4Q3HVUZkkYgVRk5M2sA")
-                .setOAuthAccessTokenSecret("xtAyR83sMvIGzsNv45S708texWK2NtP6Qqt1vfJTSAeRT");
         TwitterFactory tf = new TwitterFactory(cb.build());
         twitter = tf.getInstance();
+        try {
+            RequestToken requestToken = twitter.getOAuthRequestToken();
+
+            System.out.println("Request token: " + requestToken.getToken());
+            System.out.println("Request token secret: " + requestToken.getTokenSecret());
+            AccessToken accessToken = null;
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            while (null == accessToken) {
+                System.out.println("Open the following URL and grant access to your account:");
+                System.out.println(requestToken.getAuthorizationURL());
+                System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
+
+                try {
+                    String pin = br.readLine();
+                    if (pin.length() > 0) {
+                        accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+                    } else {
+                        accessToken = twitter.getOAuthAccessToken();
+                    }
+                } catch (TwitterException te) {
+                    if (401 == te.getStatusCode()) {
+                        System.out.println("Unable to get the access token.");
+                    } else {
+                        te.printStackTrace();
+                    }
+                }   catch (IOException ex) {
+                    System.out.println("Error getting your pin");
+                }
+
+            }
+            //persist to the accessToken for future reference.
+            //storeAccessToken(twitter.verifyCredentials().getId(), accessToken);
+            Status status = twitter.updateStatus("Salut");
+            System.out.println("Successfully updated the status to [" + status.getText() + "].");
+            System.exit(0);
+        } catch (TwitterException | IllegalStateException ie) {
+
+            if (!twitter.getAuthorization().isEnabled()) {
+                System.out.println("OAuth consumer key/secret is not set");
+            }
+        }
+
     }
 
     @Override
@@ -29,11 +81,23 @@ public class ServiceTwitter extends Service {
 
     }
 
-    public String getTweets(String user) {
-        return null;
+    public void postTweet(String tweet) {
+        try {
+            twitter.updateStatus(tweet);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
     }
 
-    public String postTweets(String tweet) {
-        return null;
+    public void getTimeline() {
+
+    }
+
+    public void sendMessages(long id, String message) {
+        try {
+            twitter.sendDirectMessage(id, message);
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
     }
 }
