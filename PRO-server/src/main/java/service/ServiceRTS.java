effebf92bd453182f42e0099b204b38941486b56
antoine.nourazar@heig-vd.ch <antoine.nourazar@heig-vd.ch>
2019-05-21 01:44:27 +0200

    get program okay, maybe need to reformat response

--- PRO-server/src/main/java/service/ServiceRTS.java
+++ PRO-server/src/main/java/service/ServiceRTS.java
@@ -7,14 +7,10 @@ import okhttp3.Response;*/
 
 
 import okhttp3.*;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -64,80 +60,37 @@ public class ServiceRTS extends Service {
     //private String programCommand = "curl -X GET --header \"Content-Type: \" --header \"Authorization: Bearer 2dIztu6lI6iAtcfUCCB3VEnffmM1\" \"https://api.srgssr.ch/epg/v1/api/programs\"\n";
     private String programCommand = "curl -X GET --header \"Content-Type: \" --header \"Authorization: Bearer GgBC9SS1GxwwqJHbuszlcbzWRNaG\" \"https://api.srgssr.ch/epg/v1/api/schedules/day\"\n";
 
-    public ArrayList<String> getProgram() {
-        Process process = null;
-        String jsonRes = "";
+    public String getProgram() {
 
-
-        try {
-            process = Runtime.getRuntime().exec(programCommand);
-            InputStream is = process.getInputStream();
-            int b = 0;
-            while ((b = is.read()) != -1) {
-                jsonRes += ((char) b);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        ArrayList<String> result = new ArrayList<>();
+        String program = new String();
 
         try {
+            URL url = new URL("https://api.srgssr.ch/epg/v1/api/schedules/day");
 
-            JSONParser parser = new JSONParser();
-            JSONObject json = (JSONObject) parser.parse(jsonRes);
-            JSONArray schedules = ((JSONArray) json.get("schedules"));
+            // Get API request
+            String tokenHeader = "Bearer " + accessToken;
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .header("Authorization", tokenHeader)
+                    .header("content-type", "application/json")
+                    .get()
+                    .build();
 
-            Iterator<JSONObject> schedulesIt = schedules.iterator();
-
-            while (schedulesIt.hasNext()) {
-
-                JSONObject nthSchedule = schedulesIt.next();
-
-                JSONArray broadcasts = ((JSONArray) nthSchedule.get("broadcasts"));
-                Iterator<JSONObject> broadcastIt = broadcasts.iterator();
-
-                while (broadcastIt.hasNext()) {
-                    JSONObject broadcast = broadcastIt.next();
-                    String channel = (String) broadcast.get("channel");
-                    JSONObject vps = (JSONObject) broadcast.get("vps");
-                    if (channel.contains("RTS")) {
-                        result.add(channel + broadcast.get("titles").toString() + vps.get("hour") + ":" + vps.get("minute"));
-                    }
-                }
+            try (Response response = client.newCall(request).execute()) {
+                JSONObject json = new JSONObject(response.body().string());
+                program = json.toString();
             }
-
-
-        } catch (ParseException e) {
-            e.printStackTrace();
+        } catch (IOException ex) {
+            Logger.getLogger(ServiceRTS.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return program;
 
-
-        return result;
     }
+
 
     @Override
     public void connect() {
-        ArrayList<String> program = getProgram();
-        for(String prog :program ){
-            System.out.println(prog);
-        }
-
-       /* OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url("https://api.srgssr.ch/epg/v1/api/programs/")
-                .get()
-                .addHeader("authorization", "Basic T0NmWTltZGlWT0dza3BUSkxRdEtsODd2RERvVAo=")
-                .build();
-
-        try {
-            Response response = client.newCall(request).execute();
-            System.out.println(response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
 
     }
 
