f7205c3e932f6c8b7fff544aa0cfe9045ec20f07
antoine.nourazar@heig-vd.ch <antoine.nourazar@heig-vd.ch>
2019-05-21 01:23:12 +0200

    Added AccessToken getter, but error on request with it

--- PRO-server/src/main/java/service/ServiceRTS.java
+++ PRO-server/src/main/java/service/ServiceRTS.java
@@ -6,28 +6,60 @@ import okhttp3.Request;
 import okhttp3.Response;*/
 
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import okhttp3.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ServiceRTS extends Service {
 
-    public ServiceRTS() {
+    public ServiceRTS() { }
+
+    private String accessToken;
+    final private String urlToken = "https://api.srgssr.ch/oauth/v1/accesstoken?grant_type=client_credentials";
+
+    public void setToken() {
+
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        String token   = new String();
+        String answer  = new String();
+
+        try {
+            // Get Token
+            OkHttpClient client = new OkHttpClient();
+            RequestBody body = RequestBody.create(JSON, answer);
+            Request request = new Request.Builder()
+                    .url(urlToken)
+                    .header("Authorization", "Basic UFZZUzI2ZWFQcUY5Um12TzBLRmh5U3hCUHJHdFpIbWE6TEVHNE0zMXRFUzFKMXpycw==")
+                    .header("Cache-Control", "no-cache")
+                    .header("Postman-Token", "5b902520-5ebb-374d-e3e8-6430cd427742")
+                    .header("Content-Length", "0")
+                    .method("POST", body)
+                    .build();
+
+            try (Response response = client.newCall(request).execute()) {
+
+                JSONObject json = new JSONObject(response.body().string());
+                token = json.get("access_token").toString();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ServiceRTS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        // Assign Token
+        accessToken = token;
     }
 
-    private String token;
-
-    private String getTokenCommand = " curl -X POST \\\n" +
-            "  'https://api.srgssr.ch/oauth/v1/accesstoken?grant_type=client_credentials' \\\n" +
-            "  -H 'authorization: Basic UFZZUzI2ZWFQcUY5Um12TzBLRmh5U3hCUHJHdFpIbWE6TEVHNE0zMXRFUzFKMXpycw==' \\\n" +
-            "  -H 'cache-control: no-cache' \\\n" +
-            "  -H 'postman-token: 5b902520-5ebb-374d-e3e8-6430cd427742'\\\n" +
-            "  -H 'content-length: 0'";
+    public String getAccessToken() {
+        return accessToken;
+    }
 
     //private String programCommand = "curl -X GET --header \"Content-Type: \" --header \"Authorization: Bearer 2dIztu6lI6iAtcfUCCB3VEnffmM1\" \"https://api.srgssr.ch/epg/v1/api/programs\"\n";
     private String programCommand = "curl -X GET --header \"Content-Type: \" --header \"Authorization: Bearer GgBC9SS1GxwwqJHbuszlcbzWRNaG\" \"https://api.srgssr.ch/epg/v1/api/schedules/day\"\n";
