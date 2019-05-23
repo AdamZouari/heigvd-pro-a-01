35b0d6db1589152f840b3156a886dcc8f563ec1c
zedsdead95 <mr.walid.koubaa@gmail.com>
2019-05-23 14:16:48 +0200

    ajout text area pour affichage des rules et contenu des rules dans les controllers

--- PRO-client/src/main/java/connection/ClientRequest.java
+++ PRO-client/src/main/java/connection/ClientRequest.java
@@ -116,12 +116,12 @@ public class ClientRequest {
     }
 
     // the rules result for
-    public void getRulesResult(String username) throws IOException, CustomException, ProtocolException {
+    public String getRulesResult() throws IOException, CustomException, ProtocolException {
         //
         sendToServer(Protocol.CMD_GET_RES_RULES + " " + loggedUser);
         String response = reader.readLine();
         checkIfSuccess(response);
-        //TODO fetch result
+        return response;
     }
 
     // the rules content
@@ -129,7 +129,6 @@ public class ClientRequest {
         sendToServer(Protocol.CMD_GET_RULES + " " + loggedUser);
         String response = reader.readLine();
         checkIfSuccess(response);
-        // checker
         return response;
 
     }
@@ -137,8 +136,6 @@ public class ClientRequest {
 
 
     public void addRule(String ruleToSend) throws IOException, CustomException, ProtocolException {
-        // TODO est ce que swtich sur le type de classe (RuleCff ou RTS...) pour choisir quoi envoyer,
-        // TODO  ou bien envoyer tel quel et c'est le serveur qui decide comment le parser
         sendToServer(Protocol.CMD_ADD_RULE + " " + loggedUser +" " + ruleToSend);
         String response = reader.readLine();
         checkIfSuccess(response);
