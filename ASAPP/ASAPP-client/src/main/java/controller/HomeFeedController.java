075fd7006a6c98c5cd04ef68fe719264c29d851e
David Jaquet <djaquet5@gmail.com>
2019-05-24 06:28:08 +0200

    doing some stuff

--- ASAPP/ASAPP-client/src/main/java/controller/HomeFeedController.java
+++ ASAPP/ASAPP-client/src/main/java/controller/HomeFeedController.java
@@ -5,7 +5,9 @@ import exceptions.CustomException;
 import exceptions.ProtocolException;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Accordion;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TitledPane;
 
 import java.io.IOException;
 import java.net.URL;
@@ -13,23 +15,49 @@ import java.util.ResourceBundle;
 
 public class HomeFeedController implements Initializable{
 
-    @FXML
-    private TextArea rulesResult;
+    private Accordion accordion = new Accordion();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         ClientRequest cr = new ClientRequest();
 
-        try {
-            rulesResult.setText(cr.getRulesResult());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (CustomException e) {
-            e.printStackTrace();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            rulesResult.setText(cr.getRulesResult());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (CustomException e) {
+//            e.printStackTrace();
+//        } catch (ProtocolException e) {
+//            e.printStackTrace();
+//        }
+    }
 
+    private void createCFFTitledPane() {
+        TitledPane titledPane = new TitledPane();
+        // TODO : implement
+
+        accordion.getPanes().add(titledPane);
+    }
+
+    private void createWeatherTitledPane() {
+        TitledPane titledPane = new TitledPane();
+        // TODO : implement
+
+        accordion.getPanes().add(titledPane);
+    }
+
+    private void createTwitterTitledPane() {
+        TitledPane titledPane = new TitledPane();
+        // TODO : implement
+
+        accordion.getPanes().add(titledPane);
+    }
+
+    private void createRTSTitledPane() {
+        TitledPane titledPane = new TitledPane();
+        // TODO : implement
+
+        accordion.getPanes().add(titledPane);
     }
 }
